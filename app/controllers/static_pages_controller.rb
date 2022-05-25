3f1da81a462fae5e0f4601934d67ddaa7b3665ce
PlaustralCL <plaustralcl@gmail.com>
2022-05-25 07:43:01 -0400

    Refactor FlickrInterface
    Changed class methods to instance methods. This allowed many of
    the methods to be kept private and limit the public interface.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 
 class StaticPagesController < ApplicationController
   def index
-    image_list = FlickrInterface.photo_search
-    @photos = FlickrInterface.get_photos(image_list)
+    test_id = "31779113@N06"
+    empty_id = "195750683@N08"
+    user_id = test_id
+    @photos = FlickrInterface.new.photos(user_id)
   end
 end
--- app/lib/flickr_interface.rb
+++ app/lib/flickr_interface.rb
@@ -3,10 +3,17 @@ require "json"
 
 class FlickrInterface
 
-  def self.photo_search
+  def photos(user_id)
+    search_results = photo_search(user_id)
+    get_photos(search_results)
+  end
+
+  private
+
+  def photo_search(user_id)
     params = { :method => "flickr.photos.search",
                :api_key => Rails.application.credentials.flickrapi[:key],
-               :user_id => "31779113@N06",
+               :user_id => user_id,
                :format => "json",
                :nojsoncallback => 1 }
 
@@ -17,16 +24,17 @@ class FlickrInterface
     image_list["photos"]["photo"]
   end
 
-  def self.photo_uri(image_details)
+  def photo_uri(image_details)
     server = image_details["server"]
     photo_id = image_details["id"]
     photo_secret = image_details["secret"]
     URI("https://live.staticflickr.com/#{server}/#{photo_id}_#{photo_secret}_q.jpg").to_s
   end
 
-  def self.get_photos(image_list)
-    image_list.map { |image| photo_uri(image) }
+  def get_photos(search_results)
+    search_results.map { |image| photo_uri(image) }
   end
+
 
 
 end
