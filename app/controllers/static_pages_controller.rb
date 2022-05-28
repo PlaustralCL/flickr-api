c5378908c73c111096936097aa7242c73cbd562e
PlaustralCL <plaustralcl@gmail.com>
2022-05-27 20:30:50 -0400

    Add basic search form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,9 @@ class StaticPagesController < ApplicationController
     test_id = "31779113@N06"
     empty_id = "195750683@N08"
     user_id = test_id
-    @photos = FlickrInterface.new.photos(user_id)
+    if params[:user_id]
+      @user_id = params[:user_id]
+      @photos = FlickrInterface.new.photos(params[:user_id])
+    end
   end
 end
