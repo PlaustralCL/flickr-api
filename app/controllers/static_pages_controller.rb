9e9e3e1c9b19cd23a5124f5d93f4f17e1c58f19b
PlaustralCL <plaustralcl@gmail.com>
2022-05-28 20:48:52 -0400

    Style the photo grid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,6 @@
 
 class StaticPagesController < ApplicationController
   def index
-    test_id = "31779113@N06"
-    empty_id = "195750683@N08"
-    user_id = test_id
     @sample_id = lucky_id
     if params[:user_id]
       @user_id = params[:user_id]
